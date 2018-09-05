4dd478f76028c701870c50c16af943aa2ae329e5
Eugen Gross <mail@eugen.cc>
2018-09-06 00:39:13 +0200

    playing with webmvc

--- web/src/main/java/cc/eugen/ph/customer/CustomerApplication.java
+++ web/src/main/java/cc/eugen/ph/customer/CustomerApplication.java
@@ -3,11 +3,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"cc.eugen.ph.customer", "cc.eugen.ph.customer.service"})
 @EntityScan("cc.eugen.ph.customer.model")
 @EnableJpaRepositories("cc.eugen.ph.customer.model")
+@PropertySource(value = {"application.properties", "webapp.properties"})
 public class CustomerApplication {
 
 	public static void main(String[] args) {
